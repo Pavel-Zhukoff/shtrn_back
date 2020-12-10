b1b2b822d4ba1c38105f2852bb88cd89fd4d5000
Apppie-dev <74676928+Apppie-dev@users.noreply.github.com>
2020-12-10 12:59:02 +0200

    added layout wrapper with styles

--- app/static/module/src/js/module.js
+++ app/static/module/src/js/module.js
@@ -1,14 +1,10 @@
 $(document).ready(function () {
     let x = 0;
     let y = 0;
-
-
-
-    $(document).click(function (e){
+    $('#layout').click(function (e){
         x = e.pageX;
         y = e.pageY;
         let coords = new Coords(x,y)
         console.log(coords.getCoords())
     })
-
 })
