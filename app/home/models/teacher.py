53487bc2e516228ac7ec7e5f99d639c964a0d132
Pavel-Zhukoff <pavel.zhuko.ff@yandex.ru>
2020-12-21 13:23:57 +0300

    fix application starter, admin templates and update teacher model

--- app/home/models/teacher.py
+++ app/home/models/teacher.py
@@ -8,17 +8,22 @@ class TeacherModel(models.Model):
     experience = models.TextField('Стаж')
     education = models.TextField('Образование')
     photo = models.ImageField('Фото', upload_to=r'teachers/%Y/%m/%d/')
-    sex = models.CharField('Пол', max_length=1, choices=(('f', 'Жен.'), ('m', 'Муж.'),), default='f')
-
-    def __str__(self):
-        return self.name
-
+    sex = models.CharField('Пол', max_length=1,
+                           choices=(('f', 'Жен.'), ('m', 'Муж.'),),
+                           default='f')
     user = models.OneToOneField(account.models.User,
                                 on_delete=models.CASCADE,
                                 primary_key=True)
 
-    class Meta:
+    @property
+    def name(self):
+        return self.user.get_full_name()
 
+    def __str__(self):
+        return self.user.get_full_name()
+
+
+    class Meta:
         db_table = 'teachers'
         verbose_name = 'преподаватель'
         verbose_name_plural = 'преподаватели'
