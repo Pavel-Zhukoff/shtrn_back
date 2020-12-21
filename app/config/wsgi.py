d6988e8cd6952b1d34dd43593ea9b4cb4e63b401
Pavel-Zhukoff <pavel.zhuko.ff@yandex.ru>
2020-12-21 16:14:12 +0300

    added config.wsgi socketio start

--- app/config/wsgi.py
+++ app/config/wsgi.py
@@ -9,8 +9,16 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 
+import socketio
 from django.core.wsgi import get_wsgi_application
+
+from config import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_wsgi_application()
+if settings.DEBUG:
+    sio = socketio.Server(logger=True, ngineio_logger=True, async_mode='gevent')
+else:
+    sio = socketio.Server(async_mode='gevent')
+
+application = socketio.WSGIApp(sio, get_wsgi_application())
